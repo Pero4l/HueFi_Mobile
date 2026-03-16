5a8b707fd020bf8f611f7cf0975d98ddf84904f5
pero4l <lilpero03@gmail.com>
2026-03-17 00:10:47 +0100

    working on users create

--- backend/controllers/usersController.js
+++ backend/controllers/usersController.js
@@ -1,2 +1,18 @@
 const { Users } = require("../models");
 
+async function usersCreation(req, res) {
+
+    const { username, email, password } = req.body;
+
+    if (!username || !email || !password) {
+        return res.status(400).json({ error: "All fields are required" });
+    }
+
+    try {
+        
+        const user = await Users.create({ username, email, password });
+        res.status(201).json(user);
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+}
