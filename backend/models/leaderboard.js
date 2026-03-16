27406337eba3f37b3cb5cba25bcec85f182906c4
pero4l <lilpero03@gmail.com>
2026-03-17 00:24:09 +0100

    working on the leaderboard

---
+++ backend/models/leaderboard.js
@@ -0,0 +1,27 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class leaderboard extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  leaderboard.init({
+    user_id: DataTypes.UUID,
+    username: DataTypes.STRING,
+    address: DataTypes.STRING,
+    points: DataTypes.INTEGER,
+    rank: DataTypes.INTEGER,
+  }, {
+    sequelize,
+    modelName: 'leaderboard',
+  });
+  return leaderboard;
+};
