5538895d3eb03bc8ca2d0600eed1e82d8b0c6cde
pero4l <lilpero03@gmail.com>
2026-03-23 23:33:25 +0100

    woring on the contract

--- contract/user/user.js
+++ contract/user/user.js
@@ -1,21 +1,21 @@
-const bip39 = require("bip39");
-const { Keypair } = require("@solana/web3.js");
-const { derivePath } = require("ed25519-hd-key");
+// const bip39 = require("bip39");
+// const { Keypair } = require("@solana/web3.js");
+// const { derivePath } = require("ed25519-hd-key");
 
-// Step 1: Generate mnemonic (12 words)
-const mnemonic = bip39.generateMnemonic();
+// // Step 1: Generate mnemonic (12 words)
+// const mnemonic = bip39.generateMnemonic();
 
-// Step 2: Convert mnemonic → seed
-const seed = bip39.mnemonicToSeedSync(mnemonic);
+// // Step 2: Convert mnemonic → seed
+// const seed = bip39.mnemonicToSeedSync(mnemonic);
 
-// Step 3: Derive path (Solana standard)
-const path = "m/44'/501'/0'/0'";
+// // Step 3: Derive path (Solana standard)
+// const path = "m/44'/501'/0'/0'";
 
-// Step 4: Get derived seed
-const derivedSeed = derivePath(path, seed.toString("hex")).key;
+// // Step 4: Get derived seed
+// const derivedSeed = derivePath(path, seed.toString("hex")).key;
 
-// Step 5: Create wallet
-const wallet = Keypair.fromSeed(derivedSeed);
+// // Step 5: Create wallet
+// const wallet = Keypair.fromSeed(derivedSeed);
 
 // Outputs
 // console.log("Mnemonic:", mnemonic);
