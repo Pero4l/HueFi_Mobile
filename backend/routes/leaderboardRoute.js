cfaa947e05512d0c4a7a2b1ad5b2b6bccd67e65b
pero4l <lilpero03@gmail.com>
2026-03-23 11:59:26 +0100

    working on the backend

---
+++ backend/routes/leaderboardRoute.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const router = express.Router();
+
+const {getLeaderboard} = require('../controllers/leaderboardController');
+
+router.get("/leaderboard", getLeaderboard);
+
+module.exports = router;
